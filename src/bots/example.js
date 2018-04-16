3688ab18cff80090e28c60877bfa73d777264de9
Viktor Hesselbom <viktor@hesselbom.net>
2018-04-16 14:49:43 +0200

    Clearer example comments

--- src/bots/example.js
+++ src/bots/example.js
@@ -13,10 +13,16 @@ return {
   name: 'My Bot',
   image: \`https://robohash.org/\${Math.random()}.png?set=set2&size=40x40&bgset=bg2\`,
 
-  // action() runs every frame
-  // Return a direction to either
-  // move your bot or shoot a bullet
-  // Return either MOVE(x,y) or SHOOT(x,y)
+  /*
+  * action() runs every frame
+  * Return a direction to either move your bot or shoot a bullet
+  *
+  * @param {name, health, pos, lastAction} me
+  * @param [{name, health, pos, lastAction}] enemies
+  * @param [{pos, vel, dir}] bullets
+  * @param int frame
+  * @returns MOVE(x,y) or SHOOT(x,y)
+  */
   action: ({ me, enemies, bullets, frame }) => {
     if (frame % 10 === 0) return MOVE(0, 1)
     return MOVE(1, 0)
